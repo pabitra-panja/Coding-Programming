0ea1d77a3935c23e2a2281648129166a886cc27d
pabitra-panja <pabitrapanja544@gmail.com>
2024-08-16 18:43:06 +0530

    first commit

---
+++ web.js
@@ -0,0 +1,9 @@
+function shownav(){
+    var show = document.querySelector('.hide')
+    show.style.display='flex'
+}
+
+function hidenav(){
+    const hide = document.querySelector('.hide')
+    hide.style.display='none'
+}
